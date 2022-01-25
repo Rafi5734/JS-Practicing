7d500a562c327c1b876affc1e0846adb4855f868
Rafi5734 <kamrulhasun5734@gmail.com>
2022-01-25 12:34:44 +0600

    ninthEx done

---
+++ eighthEx.js
@@ -0,0 +1,12 @@
+// const theName = document.getElementById("name");
+
+// const nameBtn = () => {
+//     var confirmBtn = window.confirm("Do you want to write your name?");
+//     if (confirmBtn) {
+//         const yourName = window.prompt("Please enter your name");
+
+//         theName.innerText = yourName;
+//     } else {
+//         alert("You press Cancle Button");
+//     }
+// };
