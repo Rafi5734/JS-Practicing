4b8045753b796d5ce0bd3f7088870baaea9975ba
Rafi5734 <kamrulhasun5734@gmail.com>
2022-01-25 13:02:40 +0600

    ten done

--- nineEx.js
+++ nineEx.js
@@ -1,11 +1,11 @@
-const myName = "rafi";
-const myAge = 22;
-const bangladeshi = true;
+// const myName = "rafi";
+// const myAge = 22;
+// const bangladeshi = true;
 
-const haveMoney = null;
+// const haveMoney = null;
 
-const haveMarried = undefined;
+// const haveMarried = undefined;
 
-console.log(
-    `My name Type is: ${typeof myName}. My age type is: ${typeof myAge}. My nationality type is: ${typeof bangladeshi}. I have no money: ${typeof haveMoney}. My Marriage status type is: ${typeof haveMarried}.`
-);
+// console.log(
+//     `My name Type is: ${typeof myName}. My age type is: ${typeof myAge}. My nationality type is: ${typeof bangladeshi}. I have no money: ${typeof haveMoney}. My Marriage status type is: ${typeof haveMarried}.`
+// );
---
+++ tenEx.js
@@ -0,0 +1,9 @@
+// Integer Precision = Integers(numbers without a period or exponent notation) are accurate up to 15 digits.
+
+// Floating Precision = The floating type is Point number usually has a decimal point.
+
+//     NaN - Not a Number = NaN is a JavaScript reserved word indicating that a number is not a legal number.
+
+//     Infinity = Infinity(or - Infinity) is the value JavaScript will return if you calculate a number outside the largest possible number.
+
+//     Hexadecimal = JavaScript interprets numeric constants as hexadecimal if they are preceded by 0x.
