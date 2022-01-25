5474cf26cbfe10f8547baeb1e26e3737e37116fa
Rafi5734 <kamrulhasun5734@gmail.com>
2022-01-25 11:51:12 +0600

    six done

---
+++ sixthEx.js
@@ -0,0 +1,6 @@
+// const ourPlanet = "Earth";
+// const storeCurrentVisitor = "john";
+
+// console.log(
+//     `Our Planet Name is: ${ourPlanet}.  Current Visitor of the store name is: ${storeCurrentVisitor}`
+// );
