b04e3e0e2228b718601c71175427d84047dfbecd
Rafi5734 <kamrulhasun5734@gmail.com>
2022-01-25 15:55:35 +0600

    17 done

---
+++ seventeenEx.js
@@ -0,0 +1,6 @@
+////////////////////Boolean ////////////////////
+// Boolean is a datatype that returns either of two values i.e.true or false.In JavaScript, Boolean is used as a function to get the value of a variable, object, conditions, expressions, etc.in terms of true or false.
+
+/////////////////////////////Double Not (!! ) //////////////////////////////
+
+// It is possible to use a couple of NOT operators in series to explicitly force the conversion of any value to the corresponding boolean primitive. The conversion is based on the "truthyness" or "falsyness" of the value.
