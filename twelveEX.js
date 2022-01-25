fb6dd9a89121b4e1202bb52dcec83634d9525170
Rafi5734 <kamrulhasun5734@gmail.com>
2022-01-25 13:33:40 +0600

    12 done

---
+++ twelveEX.js
@@ -0,0 +1,15 @@
+// const theDigit = document.getElementById("digit").innerText;
+// var theStringP = document.getElementById("string");
+// const theNumber = document.getElementById("number");
+// const bool = document.getElementById("bool");
+// const nameBtn = () => {
+//     var theString = String(theDigit) + 111;
+//     theStringP.innerText = `The string is: ${theString}`;
+
+//     var theNumberP = Number(theDigit) + 111;
+//     theNumber.innerText = `The number is: ${theNumberP}`;
+
+//     var theBool = Boolean(theDigit);
+
+//     bool.innerText = `The Boolean is: ${theBool}`;
+// };
